862d68ded35c8265abc8a2c6fc5760ac9be83925
NatanFonte <163172165+zelita-lima@users.noreply.github.com>
2024-05-06 14:53:45 -0300

    update
    06/05

--- variavel.js
+++ variavel.js
@@ -1,12 +1,38 @@
 const n3 = 'constante'
-var nome = "rodrigo";*/variavel global*/
-let n2;*/variavel de */
-*/são as variaveis possiveis do java*/
+var nome = "rodrigo";/*variavel global*/
+let n2;/*variavel de escopo*/
+/*são as variaveis possiveis do java*/
+
+/*
+VAR
 
 var nome = "rodigo";
 if(true)
 {
-    var nome = "jesus"
+    var nome = "jesus"//aceita redeclaração//
 }
 var nome = "silva";
+console.log(nome)
+*/
+//------------------------------------------------------//
+/*
+//LET  variavel de escopo
+
+
+if(true)
+{
+    let nome = "Jesus"//não aceita redeclaração//
+    console.log(nome)
+}
+*/
+//------------------------------------------------------//
+
+//const
+
+cont nome = "constante";
+if(true)
+{
+    cont nome = "jesus"// não aceita redeclaração.não aceita sobreescrita //
+}
+nome = "silva";
 console.log(nome)
