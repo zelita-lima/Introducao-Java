0a15b0bfa4f7ebb55dd6d65be16394a2a3935bde
NatanFonte <163172165+zelita-lima@users.noreply.github.com>
2024-05-06 17:30:52 -0300

    update
    06/05

---
+++ 005-Operadores-Aridimedicos.js
@@ -0,0 +1,52 @@
+    /*
+    adição = +
+    subtração = -
+    divisão = /
+    multiplicação = *
+    resto da divisão = %
+    encrementação = ++
+    descremento = --
+    expoente = **
+    */
+     
+    /*
+    //operadores aridimeticos soma 
+   let v1 = 2
+   let v2 = 3
+   let soma = v1+v2
+   console.log("O resultado da soma e :"+v1 + v2)
+   */
+
+   //------------------------------------------------------//
+
+   /*
+   //operadores aridimedicos subtração
+   let v1 = 10
+   let v2 = 5
+   let subtração = v1-v2
+   console.log("O resultado da subtração e :",v1 - v2)
+   console.log("O resultado da subtração e :"+subtração)
+   console.log("O resultado da subtração e :"+Number(v1 - v2))
+   */
+
+   //------------------------------------------------------//
+
+   /*
+   //operadores aridimedicos divisão
+   let v1 = 10
+   let v2 = 5
+   let divisão = v1/v2
+   console.log("O resultado da divisão e :",v1 / v2)
+   console.log("O resultado da divisão e :"+divisão)
+   console.log("O resultado da divisão e :"+Number(v1 / v2))
+   */
+
+   //------------------------------------------------------//
+
+   //operadores aridimedicos multiplicação
+   let v1 = 10
+   let v2 = 5
+   let multiplicação = v1*v2
+   console.log("O resultado da multiplicação e :",v1 * v2)
+   console.log("O resultado da multiplicação e :"+multiplicação)
+   console.log("O resultado da multiplicação e :"+Number(v1 * v2))
