1d65e9e7fba46e0e7c5db66bd93ca6cc1cf618f9
NatanFonte <163172165+zelita-lima@users.noreply.github.com>
2024-05-06 15:59:11 -0300

    update
    06/05

---
+++ 003-Concatenar-string.js
@@ -0,0 +1,23 @@
+/*
+var dono = "pai?"
+var dd = 61
+var telefone = 99999999999
+console.log("o cliente+dono+""tem o numero de celular("+dd+")"+telefone+)
+/*não ta rodando*/
+
+//---------------------------------------------------------------------------//
+/*
+var dono = "pai?"
+var dd = 61
+var telefone = 99999999999
+let texto = `o cliente ${dono} tem o seguinte celular ${dd} ${telefone}`
+/*tb não ta rodando*/
+
+//---------------------------------------------------------------------------//
+
+var dono = "pai?"
+var dd = 61
+var telefone = 99999999999
+let texto = `o cliente ${dono} tem o seguinte celular ${dd} ${telefone}`
+var idade = 19
+console.log(idade.toString() + 1)
---
+++ 004-Number.js
@@ -0,0 +1,7 @@
+/*
+Number
+Passeint
+Passefloat
+*/
+let idade = 18
+console.log(typeof idade , idade)
