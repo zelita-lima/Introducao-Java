d7784efc9b085531a64d2263dd4c652a5004f989
NatanFonte <163172165+zelita-lima@users.noreply.github.com>
2024-05-06 14:34:15 -0300

    update
    06/05

---
+++ variavel.js
@@ -0,0 +1,12 @@
+const n3 = 'constante'
+var nome = "rodrigo";*/variavel global*/
+let n2;*/variavel de */
+*/são as variaveis possiveis do java*/
+
+var nome = "rodigo";
+if(true)
+{
+    var nome = "jesus"
+}
+var nome = "silva";
+console.log(nome)
