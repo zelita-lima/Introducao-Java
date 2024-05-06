75de24e81f066eb1252f6d01030bd9915161aada
NatanFonte <163172165+zelita-lima@users.noreply.github.com>
2024-05-06 16:46:14 -0300

    update
    06/05

--- 004-Number.js
+++ 004-Number.js
@@ -3,5 +3,38 @@ Number
 Passeint
 Passefloat
 */
-let idade = 18
-console.log(typeof idade , idade)
+/*let idade = 18
+console.log(typeof idade , idade + 1) 
+*/
+
+//------------------------------------------------------------------//
+
+/*
+var iade = "18"
+console,console.log(type idade ,idade);//de string para numero//
+var iade 
+*/
+
+//-----------------------------------------------------------------//
+
+/*
+var number = Number(false)
+console.log(typeof number , number) 
+var number = Number(true)//quando tem duas negocinhas nao pode ser tudo minuscula//
+console.log(typeof number , number) 
+*/
+
+//----------------------------------------------------------------//
+
+/*
+//converter data para numero//
+var number = Number(new Date())
+console.log(typeof number , number) 
+*/
+
+//===============================================================//
+
+//converte usando paseint
+var n = "29"
+console.log(typeof n , n)
+console.log(typeof parseInt(n) , parseInt(n))
