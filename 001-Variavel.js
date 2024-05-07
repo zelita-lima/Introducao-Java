efa9bce5e35b8c0c5c928d6afc36d011553f5e73
zelita-lima <163172165+zelita-lima@users.noreply.github.com>
2024-05-07 14:34:25 -0300

    update
    07/05

--- 001-Variavel.js
+++ 001-Variavel.js
@@ -38,3 +38,34 @@ nome = "silva";
 console.log(nome)
 */
 //-----------------------------------------------------//
+
+// Exemplo de erro com var
+var nome = "João";
+console.log(nome); // Saída: "João"
+{
+    var nome = "Maria"; // Reatribui o valor da variável global
+    console.log(nome); // Saída: "Maria"
+}
+console.log(nome); // Saída: "Maria" (o valor foi alterado fora do bloco)
+
+// Exemplo de erro com let
+let animal = "gato";
+console.log(animal); // Saída: "gato"
+{
+    let animal = "cachorro"; // Declara uma nova variável de bloco com o mesmo nome
+    console.log(animal); // Saída: "cachorro"
+}
+console.log(animal); // Saída: "gato" (o valor não foi alterado fora do bloco)
+
+// Exemplo de erro com const
+const COR = "azul";
+console.log(COR); // Saída: "azul"
+{
+    const COR = "vermelho"; // Declara uma nova constante de bloco com o mesmo nome
+    console.log(COR); // Saída: "vermelho"
+}
+console.log(COR); // Saída: "azul" (o valor não foi alterado fora do bloco)
+
+// Erro ao tentar reatribuir uma constante (irá gerar erro)
+const IDADE = 30;
+IDADE = 40; // Isso irá gerar um erro
