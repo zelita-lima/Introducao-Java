2815496afbdd77d2f405932c61da6ba6beb8c173
zelita-lima <163172165+zelita-lima@users.noreply.github.com>
2024-05-07 17:31:44 -0300

    update
    07/05

---
+++ 006-Operadores-Atribuição.js
@@ -0,0 +1,46 @@
+/*
+    Operadores de atribuição
+    +=
+    -=
+    +<
+    /=
+*/
+//encremento
+/*
+let valor = 2
+console.log(valor)  
+valor += 1
+console.log(valor)
+*/
+
+//-------------------------------------------------------//
+
+//descremento
+/*
+let valor2 = 3
+console.log(valor2)
+valor2 -= 1
+console.log(valor2)
+*/
+
+//-------------------------------------------------------//
+
+//atribuição d e multipicação
+/*
+let valor3 = 4
+console.log(valor3)
+valor3 *= 2
+console.log(valor3)
+*/
+
+//-------------------------------------------------------//
+
+//atribuição de divisão
+/*
+let valor4 = 5
+console.log(valor4)
+valor3 /= 2
+console.log(valor4)
+*/
+
+//-------------------------------------------------------//
---
+++ 007-Operadores -Relacionais.js
@@ -0,0 +1,46 @@
+/*
+    Igual - "=="
+    Diferente - "!>"
+    Menor que - ">"
+    maior que - "<"
+    maior ou igual - ">="
+ */
+
+let a = 5;
+let b = 7;
+
+if (a == b) {
+    console.log("a é igual a b");
+} else {
+    console.log("a não é igual a b");
+}
+
+if (a != b) {
+    console.log("a é diferente de b");
+} else {
+    console.log("a não é diferente de b");
+}
+
+if (a < b) {
+    console.log("a é menor que b");
+} else {
+    console.log("a não é menor que b");
+}
+
+if (a <= b) {
+    console.log("a é menor ou igual a b");
+} else {
+    console.log("a não é menor ou igual a b");
+}
+
+if (a > b) {
+    console.log("a é maior que b");
+} else {
+    console.log("a não é maior que b");
+}
+
+if (a >= b) {
+    console.log("a é maior ou igual a b");
+} else {
+    console.log("a não é maior ou igual a b");
+}
