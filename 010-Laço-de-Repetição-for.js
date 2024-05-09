29f939d7384fe1f253bbfe364574e8c4092a0e28
zelita-lima <163172165+zelita-lima@users.noreply.github.com>
2024-05-09 16:50:26 -0300

    Update
    09/05

--- 010-Laço-de-Repetição-for.js
+++ 010-Laço-de-Repetição-for.js
@@ -18,15 +18,22 @@ for (let i = 0; i < frutas.length; i++) {
     console.log('Eu gosto de ' + frutas[i]);
 }
 
-const person = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30
+const pessoa = {
+    Nome: 'Zelita',
+    Sobrenome: 'lima',
+    Idade: 30
   };
   
-  for (let key in person) {
-    console.log(key + ': ' + person[key]);
-  }
+  
+  // Este é um loop for...in que itera sobre todas as propriedades enumeráveis de um objeto chamado 'pessoa'.
+// 'pessoa' é o nome do objeto sobre o qual estamos iterando.
+
+for (let key in pessoa) {
+    // 'key' é uma variável que irá conter o nome de cada propriedade do objeto 'pessoa' durante cada iteração do loop.
+  
+    // Este console.log() imprime o nome da propriedade atual (key) e seu valor correspondente (pessoa[key]) no console.
+    // 'key' é o nome da propriedade, enquanto 'pessoa[key]' acessa o valor da propriedade atual.
+    console.log(key + ': ' + pessoa[key])};
 
   for (let i = 0; i < 10; i++) {
     if (i === 5) {
