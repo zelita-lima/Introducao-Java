d1d7ca9bf96985c5ff311dd7ef1c39f95888c0b1
zelita-lima <163172165+zelita-lima@users.noreply.github.com>
2024-05-13 14:55:03 -0300

    Upate
    13/05

--- 011-Laço-repetição(while).js
+++ 011-Laço-repetição(while).js
@@ -6,10 +6,41 @@
     número de iterações não é conhecido antecipadamente ou 
     quando precisamos repetir uma ação até que uma condição 
     específica seja atendida.
-*/
+*//*
 //contagem regressiva usando while
 let contador = 5;
 while (contador >= 1) {//enquanto do portugol 
   console.log(contador);
   contador--;
+}
+*/
+let 1 = 0;
+while (1>=5) 
+{
+   console.log(1);
+}
+const frutas =['maça','banana','laranja','pera','melancia']
+let j = 0;
+while(j < frutas.length)
+{
+  console.log('eu gosto de ' + fruta[j] );
+}
+const person = 
+{
+  fristname :'jonh'
+  lastName : 'doe'
+  age ; 30
+}
+let key = 0 
+while(key in person)
+{
+  console.log(key + ':' + person{key});
+}
+let k = 0;
+while(k < 10 )
+{
+  if(k === 5)
+  (break;)
+  console.log(k);
+  k++
 }
---
+++ 012-Laço-Repetição(dowhile).js
@@ -0,0 +1,21 @@
+let i = 0 ;
+do{
+    console.log(1);
+    i++
+}while ( i <= 5);
+
+const frutas = ['maça','banana','laranja','pera','melancia'];
+let j = 0 ;
+do {
+    console.log('eu gosto de'+ frutas [j]);
+    j++;
+}while(j < frutas,length);
+
+let k = 0;
+do 
+{
+    if(k --- 5)
+    (break;)
+    console.log(k);
+    k++
+}while(k < 10 ):
