ecee5dea559e0809b5a72a67870c458acc824a6f
NatanFonte <163172165+zelita-lima@users.noreply.github.com>
2024-05-16 16:29:07 -0300

    Update
    16/05

---
+++ 024-Obejto-de-função.js
@@ -0,0 +1,31 @@
+//Definição de uma Função Construtora:
+function Pessoa() {
+    this.nome
+    this.idade;
+    this.estado_civil;
+}
+let pessoa1 = new Pessoa();
+
+pessoa1.nome = "Rodrigo"
+pessoa1.idade = 30
+pessoa1.estado_civil = "Solteiro"
+
+// Acessando propriedades dos objetos
+console.log(pessoa1.nome);
+console.log(pessoa1.idade); 
+console.log(pessoa1.estado_civil); 
+
+//Definição de uma Função Construtora:
+function Pessoa1(nome, idade, estado_civil) {
+    this.nome = nome;
+    this.idade = idade;
+    this.estado_civil = estado_civil;
+}
+
+
+let pessoa2 = new Pessoa1("Rodrigo, 38, solteiro");
+
+
+
+// Acessando propriedades dos objetos
+console.log(pessoa2);
